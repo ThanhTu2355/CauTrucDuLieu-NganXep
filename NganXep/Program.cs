f2cfdd86a924cb922b815ca6f54de7d54418cd2b
nguyentu1112005 <nguyentu1112005@gmail.com>
2024-01-23 19:22:04 +0700

    code hoan thien

--- NganXep/Program.cs
+++ NganXep/Program.cs
@@ -10,7 +10,61 @@ namespace NganXep
     {
         static void Main(string[] args)
         {
+            int socoso; // số của cơ số
+            int coso;
+            int cosodoi; // cơ số muốn đổi
+
+            Console.Write("Nhap so truoc khi doi: ");
+            socoso = int.Parse(Console.ReadLine());
+            Console.Write("Nhap co so truoc khi doi: ");
+            coso = int.Parse(Console.ReadLine());
+            Console.Write("Nhap co so muon doi: ");
+            cosodoi = int.Parse(Console.ReadLine());
+
+            Stack s = new Stack();
+            while (socoso > 0)
+            {
+                s.Them(socoso % cosodoi);
+                socoso = socoso / cosodoi;
+            }
+
+            Console.Write($"Bieu dien co so he {cosodoi}: ");
+            while (!s.KiemTraRong())
+            {
+                int n = s.LayPhanTu();
+
+                if (n > 9)
+                {
+                    Console.Write(DoiCoSoLonHon10(n));
+                }
+                else
+                {
+                    Console.Write(n);
+                }
+            }
+
             Console.ReadLine();
+        }
+
+        static char DoiCoSoLonHon10(int n)
+        {
+            switch (n)
+            {
+                case 10:
+                    return 'A';
+                case 11:
+                    return 'B';
+                case 12:
+                    return 'C';
+                case 13:
+                    return 'D';
+                case 14:
+                    return 'E';
+                case 15:
+                    return 'F';
+                default:
+                    return (char)('0' + n);
+            }
         }
     }
 }
---
+++ NganXep/Stack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NganXep
+{
+    internal class Stack
+    {
+        //khai bao thanh phan du lieu
+        private int[] data;
+        private int top;
+        //dinh nghia cac phuong thuc khoi tao
+        public Stack()
+        {
+            data = new int[100];
+            top = -1;
+        }
+        public Stack(int sophantu)
+        {
+            data = new int[sophantu];
+            top = -1;
+        }
+        //phuong thuc kiem tra rong
+        public bool KiemTraRong()
+        {
+            bool kq = false;
+            if(top == -1)
+                kq = true;
+            return kq;
+        }
+        //phuong thuc kiem tra day
+        public bool KiemTraDay()
+        {
+            bool kq = false;
+            if(top == data.Length-1)
+                kq = true;
+            return kq;
+        }
+        //them phan tu vao ngan xep
+        public void Them(int n)
+        {
+            if(KiemTraDay())
+            {
+                Console.WriteLine(" Stack da day. Khong the them ");
+            }else
+            {
+                data[++top] = n;
+            }
+        }
+        public int LayPhanTu()
+        {
+            int kq = 0;
+            if(KiemTraRong())
+            {
+                Console.WriteLine("Stack rong. Khong the lay phan tu ");
+            }else
+            {
+                kq = data[top--];
+            }
+            return kq;
+        }
+    }
+}
